0fb2cd05756910b2ae8d4d5c66c00bff412c1beb
Wendy S <wstaub@fastmail.fm>
2018-02-12 16:11:56 -0500

    Blog 1-28 no data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180212192038) do
+ActiveRecord::Schema.define(version: 20180212210608) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "author"
+    t.text "comment_entry"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
